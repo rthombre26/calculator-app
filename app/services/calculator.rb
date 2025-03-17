acd4f408a3cad3758ba40f15e762ab8051235abc
Rashmi Thombre <rashmi.thombre@m360research.com>
2025-03-17 23:15:52 +0530

    Basic Calculator tests - add, subtract, multiply and divide

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -2,4 +2,19 @@ class Calculator
   def add(a, b)
     a + b
   end
+
+  def subtract(a, b)
+    a - b
+  end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    raise ZeroDivisionError, "Cannot divide by zero" if b.zero?
+    
+    a / b
+  end
+
 end
--- spec/services/calculator_spec.rb
+++ spec/services/calculator_spec.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Calculator do
-	describe "#add" do	
-    it "adds two numbers" do
-      calculator = Calculator.new
-      result = calculator.add(2, 3)
-      expect(result).to eq(5)
-    end
+RSpec.describe Calculator do	
+  it "adds two numbers" do
+    calculator = Calculator.new
+    result = calculator.add(2, 3)
+    expect(result).to eq(5)
+  end
+
+  it "subtracts two numbers" do
+    expect(Calculator.new.subtract(5, 3)).to eq(2)
+  end
+
+  it "multiply two numbers" do
+    expect(Calculator.new.multiply(4, 3)).to eq(12)
+  end
+
+  it "divide two numbers" do
+    expect(Calculator.new.divide(10, 5)).to eq(2)
+  end
+
+  it "raises an error when dividing by zero" do
+    expect { Calculator.new.divide(10, 0) }.to raise_error(ZeroDivisionError, "Cannot divide by zero")
   end
 end
